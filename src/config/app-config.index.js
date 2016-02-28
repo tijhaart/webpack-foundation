556afe336ac680fd2b58a2912a7e12a128cdaf81
Raymond Tijhaar <contact@raymond.tijhaar.com>
2016-02-28 20:58:17 +0100

    Only load optional config file while in development

--- src/config/app-config.index.js
+++ src/config/app-config.index.js
@@ -1,13 +1,15 @@
 import Config from './app-config.module';
 import './base.config';
 
-/**
- * Optionally include a file
- */
-((_require) => {
-  if (_require.keys().indexOf('./local.config.js') > -1) {
-    _require('./local.config.js');
-  }
-})(require.context('./', false, /local\.config\.js$/));
+if (process.env.NODE_ENV === 'development') {
+  /**
+   * Optionally include a file
+   */
+  ((_require) => {
+    if (_require.keys().indexOf('./local.config.js') > -1) {
+      _require('./local.config.js');
+    }
+  })(require.context('./', false, /local\.config\.js$/));
+}
 
 export default Config;
