36ed182ea54b971a609bf92e3d2e2e2bde586041
Raymond Tijhaar <contact@raymond.tijhaar.com>
2016-03-09 01:44:49 +0100

    Created the initial todo app with redux

--- src/app.js
+++ src/app.js
@@ -1,55 +1,10 @@
-// import AppConfig from 'config/app-config.index';
 import angular from 'angular';
-
-import ngUIRouter from 'angular-ui-router';
-import ngRedux from 'ng-redux';
-import {combineReducers} from 'redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-import ngReduxRouter from 'redux-ui-router';
-import {
-  router,
-  stateGo,
-  stateReload,
-  stateTransitionTo
-} from 'redux-ui-router';
-
-const ngModule = angular
-  .module('foundationTodo', [
-    ngUIRouter,
-    ngRedux,
-    ngReduxRouter
-  ])
-  .config(function($ngReduxProvider) {
-    'ngInject';
-
-    const logger = createLogger({
-      level: 'info',
-      collapsed: true
-    });
-
-    const reducers = combineReducers({
-      router
-    });
-
-    $ngReduxProvider.createStoreWith(
-      reducers,
-      [
-        'ngUiRouterMiddleware',
-        logger,
-        thunk
-      ]
-    );
-  })
-;
-
-// import './modules/ng-redux-spike';
+import foundationTodo from './modules/foundation-todo/foundation-todo.index';
 
 angular.element(document).ready(() => {
   'use strict';
   angular.bootstrap(document.body, [
-    'foundationTodo'
-    // 'ngReduxSpike'
+    foundationTodo.name
   ], {
     // Forces strict dependency injection
     // see: https://docs.angularjs.org/api/ng/function/angular.bootstrap
