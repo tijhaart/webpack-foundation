cab15b1452313777aa8a13ef7cce28195faa7067
Raymond Tijhaar <contact@raymond.tijhaar.com>
2016-03-22 16:05:45 +0100

    Format code and clean up

--- asset.server.js
+++ asset.server.js
@@ -1,12 +1,8 @@
 /* eslint strict: 0, no-console: 0 */
-'use strict';
 
 import path from 'path';
-import fs from 'fs';
-import _ from 'lodash';
 import util from 'util';
 import webpack from 'webpack';
-import webpackDevServer from 'webpack-dev-server';
 import config from './webpack.config.js';
 import express from 'express';
 import devMiddleware from 'webpack-dev-middleware';
@@ -18,32 +14,37 @@ let compiler = webpack(config);
 app.use(devMiddleware(compiler, config.devServer));
 app.use(hotMiddleware(compiler));
 
-const {host, port} = config.devServer;
+const {
+  host,
+  port
+} = config.devServer;
 
-// @TODO don't send 403 when env.production is true
 app.get(/\.(html|css)$/i, (req, res) => res.sendStatus(403));
 
 app.get('*', (req, res, next) => {
-	var filename = path.join(compiler.outputPath,'index.html');
+  var filename = path.join(compiler.outputPath, 'index.html');
   compiler.outputFileSystem.readFile(filename, (err, result) => {
     if (err) {
-			console.log(util.inspect(
-				`[asset.server] Unable to find "${filename}". Did you configure/use html-webpack?`, {depth: 1, colors: true}
-			));
+      console.log(util.inspect(
+        `[asset.server] Unable to find "${filename}". Did you configure/use html-webpack?`, {
+          depth: 1,
+          colors: true
+        }
+      ));
       return next(err);
     }
 
-    res.set('content-type','text/html');
+    res.set('content-type', 'text/html');
     res.send(result);
     res.end();
   });
 });
 
 app.listen(port, host, err => {
-	if (err) {
-		console.log(err);
-		return;
-	}
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-	console.log(`\n >> Listening at http://${host}:${port}\n`);
+  console.log(`\n >> Listening at http://${host}:${port}\n`);
 });
