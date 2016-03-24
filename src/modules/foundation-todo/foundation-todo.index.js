f209b2ef7a56df87523e7f500fa512624bbb2c0a
Raymond Tijhaar <contact@raymond.tijhaar.com>
2016-03-24 22:42:49 +0100

    Removed commented out code

--- src/modules/foundation-todo/foundation-todo.index.js
+++ src/modules/foundation-todo/foundation-todo.index.js
@@ -200,35 +200,4 @@ export default angular
   .component('todoApp', {
     templateUrl: require('./todo-app.ngtpl.html')
   })
-  // .component('todoItem', {
-  //   bindings: {
-  //     item: '<'
-  //   },
-  //   controller($ngRedux) {
-  //     'ngInject';
-  //     const ctrl = this;
-  //
-  //     ctrl.toggleCompleted = (event) => {
-  //       event.preventDefault();
-  //       toggleCompleted();
-  //     };
-  //
-  //     ctrl.remove = (event) => {
-  //       event.preventDefault();
-  //
-  //       $ngRedux.dispatch({
-  //         type: 'REMOVE_TODO_ITEM',
-  //         payload: ctrl.item.id
-  //       });
-  //     };
-  //
-  //     function toggleCompleted() {
-  //       $ngRedux.dispatch({
-  //         type: 'TODO_ITEM_TOGGLE_COMPLETED',
-  //         payload: ctrl.item
-  //       });
-  //     }
-  //   },
-  //   templateUrl: require('./todo-app.todo-item.ngtpl.html')
-  // })
 ;
